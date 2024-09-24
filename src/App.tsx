c3068ce926f7d9917f93bf7fa141166218321ba3
Samuel <slarico94@gmail.com>
2024-09-23 23:37:47 -0500

    homepage added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to the fullstack course</h1>
-    </div>
+    <>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </>
   );
 }
-
-export default App;
---
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export const Navbar = () => {
+    return (
+        <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+            <div className='container-fluid'>
+                <span className='navbar-brand'>Luv 2 Read</span>
+                <button className='navbar-toggler' type='button'
+                    data-bs-toggle="collapse" data-bs-target='#navbarNavDropdown'
+                    aria-controls='navbarNavDropdown' aria-expanded="false"
+                    aria-label='Toggle Navigation'>
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item'>
+                            <a href="#" className='nav-link'>Home</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href="#" className='nav-link'>Search books</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href="#" className='nav-link'>Perfect</a>
+                        </li>
+                    </ul>
+                    <ul className='navbar-nav ms-auto'>
+                        <li className='nav-item m-1'>
+                            <a type='button' href='#' className='btn btn-outline-light'>Sign in</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
