d1211688d6456524d0bc3217d8e7c8b703991bec
Samuel <slarico94@gmail.com>
2024-09-25 23:28:02 -0500

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,23 @@ import { HomePage } from './layouts/HomePage/HomePage';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <>
       <Navbar />
-      { /* HomePage />*/}
-      <SearchBooksPage />
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/home'>
+          <HomePage />
+        </Route>
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </>
   );
