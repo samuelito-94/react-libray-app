6fda1d5b6cfdfa781eb233b3fc46c558ee00ee93
Samuel <slarico94@gmail.com>
2024-10-23 11:03:16 -0500

    add auth logic to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,25 @@ import { HomePage } from './layouts/HomePage/HomePage';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
+import { OktaConfig } from './lib/okta-config';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
+
+const oktaAuth = new OktaAuth(OktaConfig);
 
 export const App = () => {
+
+  const history = useHistory();
+
+  const customAuthHandler = () => {
+    history.push('/login');
+  }
+
+  const restoreOriginalUri = async (_okAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  }
+
   return (
     <div className='d-flex flex-column min-vh-100'>
       <Navbar />
