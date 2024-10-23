f84f55e03f0ab92dd539ed6292778da005919403
Samuel <slarico94@gmail.com>
2024-10-23 11:17:59 -0500

    logout functionality

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,7 +1,20 @@
+import { useOktaAuth } from '@okta/okta-react';
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { SpinnerLoading } from '../Utils/SpinnerLoading';
 
 export const Navbar = () => {
+
+    const { oktaAuth, authState } = useOktaAuth();
+
+    if (!authState) {
+        return <SpinnerLoading />
+    }
+
+    const handleLogout = async () => oktaAuth.signOut();
+
+    console.log(authState)
+
     return (
         <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
             <div className='container-fluid'>
@@ -22,9 +35,15 @@ export const Navbar = () => {
                         </li>
                     </ul>
                     <ul className='navbar-nav ms-auto'>
-                        <li className='nav-item m-1'>
-                            <NavLink type='button' to='/login' className='btn btn-outline-light'>Sign in</NavLink>
-                        </li>
+                        {authState.isAuthenticated ?
+                            <li>
+                                <button className='btn btn-outline-light' onClick={handleLogout}>Logout</button>
+                            </li> :
+                            <li className='nav-item m-1'>
+                                <Link type='button' to='/login' className='btn btn-outline-light'>Sign in</Link>
+                            </li>
+                        }
+
                     </ul>
                 </div>
             </div>
